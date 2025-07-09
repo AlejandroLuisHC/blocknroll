e09ab4213b19ceef7cc316d9373383d01f945c74
AlejandroLuisHC <alejandrolhc@gmail.com>
2025-07-09 22:00:59 +0200

    refactor: Remove unused font loader utility and associated tests; update font references in styles and HTML

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n";
-import "./utils/fontLoader";
 import App from "./App";
 
 // Get the root element and ensure it exists
